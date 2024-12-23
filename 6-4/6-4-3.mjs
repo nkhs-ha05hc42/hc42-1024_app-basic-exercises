fb123bf2b37626c89b60c8dbe240aa12cd97233f
masuo338 <nkhshc421024@g.neec.ac.jp>
2024-12-23 16:19:14 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,9 @@
+const score = 66
+if (score > 90){
+    console.log("Excellent")
+}else if (score > 80){
+    console.log("good")
+}else if (score > 60){
+    console.log("Normal")
+}else
+console.log("Red")
